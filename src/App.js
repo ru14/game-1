4cf4ccee6dba2723f092104c14a58d69eaa22edc
Raginee Upadhyaya <upadhyayaraginee@gmail.com>
2022-03-15 17:07:13 -0700

    Managing a list of USERS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserList) => {
+      return [...prevUserList, {name: uName, age: uAge, id:Math.random().toString()}];  //spread operater pull out all the element from the array and as new elwment to this new array
+    })
+  }
   return (
     <div>
-<AddUser/>
-<UsersList users = {[]}/>
+<AddUser onAddUser = {addUserHandler}/>
+<UsersList users = {usersList}/>
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Card from '../UI/Card';
 import classes from './AddUser.module.css'
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -15,19 +16,21 @@ const AddUser = (props) => {
     }
 
     const formSubmitHandler = event => {
-        if(enteredUsername.trim().length === 0 || enteredUserAge.trim().length === 0){
+        if (enteredUsername.trim().length === 0 || enteredUserAge.trim().length === 0) {
             return;
         }
 
-        if (+enteredUserAge < 1){
+        if (+enteredUserAge < 1) {
             return;
         }
         event.preventDefault();
-        console.log(event)
+        props.onAddUser(enteredUsername, enteredUserAge);
         setEnteredUsername('');
         setEnteredUserAge('');
     }
     return (
+        <div>
+        <ErrorModal title = "An error Occured" message="Something went wrong"/>
         <Card className={classes.input}>
             <form onSubmit={formSubmitHandler}>
                 <label htmlFor="username">UserName</label>
@@ -45,6 +48,7 @@ const AddUser = (props) => {
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
+        </div>
     )
 }
 
